7b5463ccd971ffaf01a4ec8053a1893c950c5f02
Will Richardson <rich1051@umn.edu>
2023-05-18 13:32:12 -0500

    added pool.js and connected exported pool

---
+++ server/modules/pool.js
@@ -0,0 +1,12 @@
+const pg = require('pg'); 
+
+const pool = new pg.Pool({
+    // name of our database:
+        database: 'pglecture',
+    // where our database is:
+        host: 'localhost', 
+    // Postgres listens on 5432 by default:
+        port: 5432
+    });
+
+module.exports = pool;
